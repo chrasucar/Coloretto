1d2f9d7941bc19b00f8fb3317a66b250b5fee784
Christian <chrasucar@alum.us.es>
2024-10-07 18:25:56 +0200

    Iteración 6: Falta 2 css

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const instance = axios.create({
 
-    baseURL: 'http://localhost:3000' || process.env.FRONTEND_URL,
+    baseURL: 'http://localhost:3000' || process.env.MONGODB_URI,
     withCredentials: true
 
 })
