fe3ad9e978ffbf64f9ef069bf34949251b90b84e
Christian <chrasucar@alum.us.es>
2024-10-15 11:26:55 +0200

    Entrega final

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const instance = axios.create({
 
-    baseURL: 'https://coloretto-api.vercel.app',
+    baseURL: 'http://coloretto-api.onrender.com',
     withCredentials: true
 
 })
