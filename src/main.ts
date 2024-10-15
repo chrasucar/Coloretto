bcb84916564ef033cbd7b397a57ceb5ddd27e1b0
Christian <chrasucar@alum.us.es>
2024-10-15 11:37:54 +0200

    Entrega final

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,6 @@ import { Strategy as JwtStrategy, ExtractJwt } from 'passport-jwt';
 import * as passport from 'passport';
 import * as cookieParser from 'cookie-parser';
 import { IoAdapter } from '@nestjs/platform-socket.io';
-import { Server } from 'socket.io';
 
 async function bootstrap() {
 
@@ -28,38 +27,7 @@ async function bootstrap() {
 
   app.use(passport.initialize());
 
-  // Adaptador de WebSocket
-
-  app.useWebSocketAdapter(
-    new (class extends IoAdapter {
-      createIOServer(port: number, options?: any): Server {
-
-        options = {
-          cors: {
-            origin: 'https://coloretto.vercel.app', 
-            methods: ['GET', 'POST'],
-            credentials: true,
-          },
-        };
-
-        const server = super.createIOServer(port, options);
-
-        server.on('connection', (socket) => {
-          console.log('Nuevo cliente conectado:', socket.id);
-
-          socket.on('disconnect', () => {
-            console.log('Cliente desconectado:', socket.id);
-          });
-
-          socket.on('connect_error', (error) => {
-            console.error('Error de conexión:', error);
-          });
-        });
-
-        return server;
-      }
-    })(app),
-  );
+  app.useWebSocketAdapter(new IoAdapter(app));
 
   const port = process.env.PORT || 3001;
 
